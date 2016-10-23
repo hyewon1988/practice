033b1767a53c12b3a72f6c0a915bb644d1f579d9
hyewon <hyewon1988@gmail.com>
2016-10-23 14:27:17 +0000

    study jsx

--- react/src/components/App.js
+++ react/src/components/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
- 
+
 class App extends React.Component {
+
+    sayHello(){
+      alert('Hello');
+    }
+
     render(){
- 
+
+      let text = 'Dev-Server';
+
+      let pStyle = {
+        color : 'yellow',
+        backgroundColor : 'black'
+      };
+
         return (
-                <h1>Hello React Skeleton</h1>
+          <div>
+                <h1>Hello Monkey</h1>
+                <h2>Welocome to Monkey {text}</h2>
+                <button onClick={this.sayHello}>Greetings</button>
+                <p style={pStyle}>{1==1 ? 'true':'false'}</p>
+          </div>
         );
     }
 }
- 
+
 export default App;
